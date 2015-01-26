035f8a77a853579f10775cd8d8d1ba88f228a93e
Tyrone Palmer <hateration@gmail.com>
2015-01-26 00:10:35 +0000

    Adding tested solutions through exercise 13

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,40 +1,80 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and
+   (empty? (rest coll))
+   (not (empty? coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? coll)
+    (first coll)
+    (if (singleton? coll)
+      (first coll)
+      (my-last (rest coll)))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (or
+   (= elem (first a-seq))
+   (and
+    (not (empty? (rest a-seq)))
+    (sequence-contains? elem (rest a-seq)))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+      '())))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (not (pred? (first a-seq)))
+      a-seq
+      (my-drop-while pred? (rest a-seq)))))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (or
+   (and (empty? a-seq) (empty? b-seq))
+   (and
+    (= (count a-seq) (count b-seq))
+    (= (first a-seq) (first b-seq))
+    (seq= (rest a-seq) (rest b-seq)))))
+
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (not (or (empty? seq-1) (empty? seq-2)))
+    (cons (f (first seq-1) (first seq-2))
+          (my-map f (rest seq-1) (rest seq-2)))
+    '()))
 
 (defn power [n k]
   :-)
